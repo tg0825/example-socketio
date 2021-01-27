59299414ada55c3897ac5436343872fb7be398bd
ted <ted@cupist.com>
2021-01-28 02:42:11 +0900

    [add] 멀티 채팅방 러프 스케치

--- socket/room_chat/app.js
+++ socket/room_chat/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // 채팅방 종류
-let room = ['room1', 'room2', 'room3'];
+let room = [];
 let a = 0;
 
 // / 일 경우 chat파일 호출
@@ -19,12 +19,30 @@ app.get('/', (req, res) => {
 
 // default에 접근시
 io.on('connection', (socket) => {
+    // 현재 방 가져오기
+    function getRoomsKey(rooms) {
+        const result = [];
+        for (var entry of rooms.entries()) {
+            var key = entry[0];
+            if (entry[0].indexOf('room') < 0) {
+                continue;
+            }
+            result.push(key);
+        }
+        return result;
+    }
+
     console.log('유저가 접속했습니다.');
 
-    // 현재 방 가져오기
-    socket.on('onGetRoom', (data) => {
-        console.log(io.sockets.adapter.rooms);
-        // socket.emit('getRoom', rooms);
+    // 전체에게 전송
+    socket.broadcast.emit('hi');
+
+    // 방 생성
+    socket.on('create', function (room) {
+        socket.join(room);
+        const rooms = io.sockets.adapter.rooms;
+        const result = getRoomsKey(rooms);
+        io.emit('setRooms', result);
     });
 
     // 소켓 연동 종료 이벤트
@@ -35,13 +53,13 @@ io.on('connection', (socket) => {
     // socket 참여 이벤트 핸들러 (이 이벤트 호출은 클라이언트에서 한다)
     socket.on('joinRoom', (num, name) => {
         a = num;
-        console.log(num);
         console.log(`[SERVER] 참여 ${num}방 ${name}님`);
-        socket.join(room[num]);
+        socket.join(num);
         // 특정 룸의 이벤트를 호출한다. (클라이언트에 이벤트를 발송한다.)
         // io는 일종의 아파트의 관리사무실이라고 보면 된다.
         // 관리사무실에서 특정 방에 방송을 하는 것이다.
-        io.to(room[num]).emit('joinChatRoom', name, num);
+        // io.to(room[num]).emit('joinChatRoom', name, num);
+        io.to(num).emit('joinChatRoom', name, num);
         
     });
 
@@ -49,16 +67,15 @@ io.on('connection', (socket) => {
     socket.on('leaveRoom', (num, name) => {
         console.log(`[SERVER] 이탈 ${num}방 ${name}님`);
         // socket에 leave
-        socket.leave(room[num]);
+        socket.leave(num);
         // 특정 룸의 이탈 이벤트 (leaveRoom)를 클라이언트로 송출한다.
-        io.to(room[num]).emit('leaveChatRoom', name, num);
+        io.to(num).emit('leaveChatRoom', name, num);
     });
 
     // chat message 이벤트 핸들러
     socket.on('chat message', (num, name, msg) => {
         a = num;
-        io.to(room[num]).emit('chat message', name, msg);
-        console.log(room[num]);
+        io.to(num).emit('chat message', name, msg);
     });
 });
 
